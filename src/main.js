256265066720067686eba338afd1178d54e0c006
ciandre1976 <ciandre23@gmail.com>
2026-03-07 18:22:59 +0100

    Remove unused executable and refactor main.js to improve structure and add input handling; update style.css for canvas border styling.

--- src/main.js
+++ src/main.js
@@ -1,25 +1,81 @@
-import './style.css'
-import player from './player.png';
-import enemy1 from './enemy_1.png';
-import background_single from './background_single.png'
+import "./style.css";
+import player from "./player.png";
+import enemy1 from "./enemy_1.png";
+import background_single from "./background_single.png";
 
-let playerImage = new Image();
-playerImage.src = background_single;
-
-let canvas = document.getElementById('canvas')
-let ctx = canvas.getContext('2d')
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 
-console.log(player, enemy1, background_single);
+canvas.width = 800;
+canvas.height = 720;
 
+class InputHandler {
+  constructor() {
+    this.keys = [];
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'ArrowDown' ||
+        'ArrowUp' ||
+        'ArrowDown' ||
+        'ArrowLeft' && this.keys.indexOf(e.key) === -1) {
+        this.keys.push(e.key);
+      }
+      console.log(this.keys);
+    })
+    window.addEventListener('keyup', (e) => {
+      if (e.key === 'ArrowDown' ||
+        'ArrowUp' ||
+        'ArrowLeft' ||
+        "ArrowRight") {
+        this.keys.splice(this.keys.indexOf(e.key), 1);
+      }
+      console.log(this.keys);
+    })
+  }
+}
 
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  ctx.drawImage(playerImage, 0, 0, canvas.width, canvas.height);
-  window.requestAnimationFrame(animate)
+class background {
+
 }
 
 
-animate();
+
+class Player {
+  constructor(gameWidth, gameHeight) {
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.height = 200;
+    this.width = 200;
+    this.x = 0;
+    this.y = 0;
+  }
+  draw(context) {
+    context.fillStyle = 'red';
+    context.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+class Enemy {
+
+}
+
+function handleEnemy() {
+}
+
+function displayStatus() {
+
+}
+
+
+const input = new InputHandler();
+let p = new Player(canvas.width, canvas.height);
+p.draw(ctx);
+
+
+function animate(timestamp) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  window.requestAnimationFrame(animate);
+}
 
 
